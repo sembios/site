2f7f39558c869e8ff8dd94e649cb1e68f506360d
Павлушко Василий <sembios@gmail.com>
2022-04-05 15:56:55 +0600

    new design big size

--- assets/js/main.js
+++ assets/js/main.js
@@ -246,26 +246,26 @@
 
 })()
 
-window.onload = function(){
-  // Щит клавиатуры событий
-document.onkeydown = function (){
-var e = window.event || arguments[0];
-//F12
-if(e.keyCode == 123){
-return false;
-//Ctrl+Shift+I
-}else if((e.ctrlKey) && (e.shiftKey) && (e.keyCode == 73)){
-return false;
-//Shift+F10
-}else if((e.shiftKey) && (e.keyCode == 121)){
-return false;
-//Ctrl+U
-}else if((e.ctrlKey) && (e.keyCode == 85)){
-return false;
-}
-};
-  // Щит правой кнопкой мыши
-document.oncontextmenu = function (){
-return false;
-}
-}
+// window.onload = function(){
+//   // Щит клавиатуры событий
+// document.onkeydown = function (){
+// var e = window.event || arguments[0];
+// //F12
+// if(e.keyCode == 123){
+// return false;
+// //Ctrl+Shift+I
+// }else if((e.ctrlKey) && (e.shiftKey) && (e.keyCode == 73)){
+// return false;
+// //Shift+F10
+// }else if((e.shiftKey) && (e.keyCode == 121)){
+// return false;
+// //Ctrl+U
+// }else if((e.ctrlKey) && (e.keyCode == 85)){
+// return false;
+// }
+// };
+//   // Щит правой кнопкой мыши
+// document.oncontextmenu = function (){
+// return false;
+// }
+// }
